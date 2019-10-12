7fba3a79ee9bb2e0e20973e59dabb8bbbf0bb21c
Samuel Unferth <sru4607@g.rit.edu>
2019-10-12 11:21:00 -0400

    Added some standard assets and created a basic scene

---
+++ New Unity Project/Assets/Code/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotation
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0, 270, 0);
+        }
+
+        //Movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            gameObject.transform.Translate(new Vector3(-speed, 0, 0), Space.World);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.Translate(new Vector3(0, 0, -speed), Space.World);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.Translate(new Vector3(speed, 0, 0), Space.World);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.Translate(new Vector3(0, 0, speed),Space.World);
+        }
+    }
+}
