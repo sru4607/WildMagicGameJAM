1ca3bb1ecca3df8c28fad27bb3bd0df74cfde500
asb9599 <asb9599@rit.edu>
2019-10-12 14:38:06 -0400

    Worked on Initial Puzzle Rooms

---
+++ New Unity Project/Assets/Code/CageControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CageControl : MonoBehaviour
+{
+    // Inspector Assigned Variables
+    [SerializeField] GameObject floorCollider;
+    [SerializeField] GameObject normalCage;
+    [SerializeField] GameObject brokenCage;
+    // Public Variables
+    public bool isBroken;
+    public bool isFalling;
+    // Private Variables
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        isBroken = false;
+        isFalling = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isBroken && !isFalling && Input.GetKeyDown(KeyCode.Space))
+        {
+            Release();
+        }
+    }
+
+    public void Release()
+    {
+        if (!isFalling && !isBroken)
+        {
+            isFalling = true;
+            rb.useGravity = true;
+        }
+
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!isBroken)
+        {
+            isBroken = true;
+            normalCage.SetActive(false);
+            brokenCage.SetActive(true);
+        }
+    }
+}
---
+++ New Unity Project/Assets/Code/DoorControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControl : MonoBehaviour
+{
+    // Inspector Assigned Variables
+    // Public Variables
+    public bool isOpen;
+    // Private Variables
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isOpen && Input.GetKeyDown(KeyCode.F))
+        {
+            Open();
+        }
+    }
+
+    public void Open()
+    {
+        if (!isOpen)
+        {
+            isOpen = true;
+            anim.SetBool("isOpen", true);
+        }
+    }
+}
