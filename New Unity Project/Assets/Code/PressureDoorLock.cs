2bd17d459d860844336875a46105e7c8c5faeeca
Lucas Szeto <42018857+lts1575@users.noreply.github.com>
2019-10-13 14:53:03 -0400

    Pressure Plates

---
+++ New Unity Project/Assets/Code/PressureDoorLock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressureDoorLock : DoorLockCheck
+{
+    public List<PressurePlate> pressurePlates;
+
+    private void Update() {
+        foreach (PressurePlate plate in pressurePlates) {
+            if (plate.pressed) {
+                Locked = true;
+            }
+            else {
+                Locked = false;
+            }
+        }
+    }
+}
---
+++ New Unity Project/Assets/PressurePlate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour{
+
+    public bool pressed;
+
+    private void Start() {
+        pressed = false;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        pressed = true;
+    }
+
+    private void OnTriggerExit(Collider other) {
+        pressed = false;
+    }
+}
