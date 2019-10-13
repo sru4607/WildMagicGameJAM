98380e85f6f4cb04150a573909c340b16542d96a
lts1575 <lts1575@rit.edu>
2019-10-13 08:07:03 -0400

    Updated Door Controls

--- New Unity Project/Assets/Code/DoorControl.cs
+++ New Unity Project/Assets/Code/DoorControl.cs
@@ -8,21 +8,24 @@ public class DoorControl : MonoBehaviour
 	public PossessedBar LockBar;
 	public DoorLockCheck leftDoor;
 	public DoorLockCheck rightDoor;
+	public float proximity;
     // Public Variables
     public bool isOpen;
 	// Private Variables
 	private bool locked;
     Animator anim;
+	private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
+		player = GameObject.FindGameObjectWithTag("Player");
         anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isOpen && !leftDoor.Locked && rightDoor.Locked && Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F) && !isOpen && PlayerNear() && !leftDoor.Locked && !rightDoor.Locked)
         {
             Open();
         }
@@ -36,6 +39,10 @@ public class DoorControl : MonoBehaviour
             anim.SetBool("isOpen", true);
         }
     }
+
+	private bool PlayerNear() {
+		return Vector3.Distance(transform.position, player.transform.position) < proximity;
+	}
 
 
 }
--- New Unity Project/Assets/Code/DoorLockCheck.cs
+++ New Unity Project/Assets/Code/DoorLockCheck.cs
@@ -7,12 +7,15 @@ public class DoorLockCheck : MonoBehaviour
 	public PossessedBar LockBar;
 	public bool Locked;
 
-
 	private void OnCollisionStay(Collision collision) {
-		if (collision.gameObject == LockBar) {
+		if (collision.gameObject == LockBar.gameObject) {
 			Locked = true;
 		} else {
 			Locked = false;
 		}
+	}
+
+	private void OnCollisionExit(Collision collision) {
+		Locked = false;
 	}
 }
