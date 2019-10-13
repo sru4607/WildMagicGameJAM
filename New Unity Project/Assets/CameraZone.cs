3165782b65b66ecbb94aee0fc39b15dd7b10f597
Lucas Szeto <42018857+lts1575@users.noreply.github.com>
2019-10-13 10:28:43 -0400

    Main Scene and Camera Controller

---
+++ New Unity Project/Assets/CameraZone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZone : MonoBehaviour {
+
+    public int ZoneNumber;
+    public CameraController cameraController;
+
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            cameraController.SetPlayerPos(ZoneNumber);
+        }
+        if (other.gameObject.tag == "Spirit") {
+            cameraController.SetSpiritPos(ZoneNumber);
+        }
+    }
+
+
+}
